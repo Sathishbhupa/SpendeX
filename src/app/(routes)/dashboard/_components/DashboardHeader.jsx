ba8909dc1294921440e6a547012fc699c88c205f
Sathish A <144202918+Sathishbhupa@users.noreply.github.com>
2025-02-04 14:46:02 +0530

    Update DashboardHeader.jsx

--- src/app/(routes)/dashboard/_components/DashboardHeader.jsx
+++ src/app/(routes)/dashboard/_components/DashboardHeader.jsx
@@ -16,7 +16,7 @@ function DashboardHeader() {
     <div className={`p-5 flex justify-between ${isDarkMode ? 'dark' : ''}`}>
       <div className="flex relative">
       <button className="shadow-lg mr-3 bg-neutral-950 rounded-full p-2 flex items-center justify-center transition-all duration-300" onClick={toggleDarkMode}>
-          <div className={`w-10 h-5 flex items-center rounded-full ${isDarkMode ? 'bg-yellow-400 : 'bg-gray-300'} transition-all duration-300`}>
+          <div className={`w-10 h-5 flex items-center rounded-full ${isDarkMode ? 'bg-lime-500' : 'bg-gray-300'} transition-all duration-300`}>
             <div className={`w-6 h-6 bg-white rounded-full items-center shadow-md transform ${isDarkMode ? 'translate-x-5' : 'translate-x-0'} transition-transform duration-300`}>
               {isDarkMode ? <Sun className="w-4 ml-1" /> : <Moon className="w-4 ml-1" />}
             </div>

d96a9b4e7faeaf7ed5a95073e54ac4067bd5de36
Sathish A <144202918+Sathishbhupa@users.noreply.github.com>
2025-02-04 12:21:04 +0530

    Update page.jsx

--- src/app/(routes)/dashboard/reports/page.jsx
+++ src/app/(routes)/dashboard/reports/page.jsx
@@ -1,1 +1,12 @@
+import React from 'react'
+import BudgetList from './_components/Monthlyreport'
 
+function Report() {
+  return (
+    <div className='p-10'>
+      <Monthlyreport />
+    </div>
+  )
+}
+
+export default Report;

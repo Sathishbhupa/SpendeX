7ea642e100fa86d743b47af65831b3b7ec56bc61
Sathish A <144202918+Sathishbhupa@users.noreply.github.com>
2025-02-07 15:05:28 +0530

    Add files via upload

--- src/app/(routes)/dashboard/expenses/[id]/loading.jsx
+++ src/app/(routes)/dashboard/expenses/[id]/loading.jsx
@@ -3,8 +3,8 @@ import React from 'react';
 function Loading() {
   return (
     <div className="flex justify-center items-center h-screen">
-      <div className="spinner-border animate-spin inline-block w-8 h-8 border-4 rounded-full text-blue-600" role="status">
-        <span className="sr-only">Loading...</span>
+      <div className="bg">
+        <div className="loader"></div>
       </div>
     </div>
   );

bab872cefe2cb7677baf53bc620c9302e4d17fa0
Sathish A <144202918+Sathishbhupa@users.noreply.github.com>
2025-02-07 12:11:03 +0530

    Add files via upload

--- src/app/(routes)/dashboard/reports/page.jsx
+++ src/app/(routes)/dashboard/reports/page.jsx
@@ -1,12 +1,41 @@
-import React from 'react'
-import BudgetList from './_components/Monthlyreport'
+"use client"
 
-function Report() {
+import { db } from '../../../../../utils/dbConfig';
+import { Budgets, Expenses } from '../../../../../utils/schema';
+import { desc, eq } from 'drizzle-orm';
+import React, { useEffect, useState } from 'react'
+import DownloadExpensesButton from '../expenses/_components/DownloadExpensesButton';
+import { useUser } from '@clerk/nextjs';
+
+function report() {
+
+  const [expensesList,setExpensesList]=useState([]);
+    const {user}=useUser();
+
+    useEffect(()=>{
+        user&&getAllExpenses();
+      },[user])
+
+  const getAllExpenses=async()=>{
+    const result=await db.select({
+      id:Expenses.id,
+      name:Expenses.name,
+      amount:Expenses.amount,
+      createdAt:Expenses.createdAt
+    }).from(Budgets)
+    .rightJoin(Expenses,eq(Budgets.id,Expenses.budgetId))
+    .where(eq(Budgets.createdBy,user?.primaryEmailAddress.emailAddress))
+    .orderBy(desc(Expenses.id));
+    setExpensesList(result);
+   
+  }
   return (
     <div className='p-10'>
-     hello
+      <h2 className='font-bold text-3xl'>Monthly Report</h2>
+
+      <DownloadExpensesButton expensesList={expensesList} />
     </div>
   )
 }
 
-export default Report;
+export default report;
